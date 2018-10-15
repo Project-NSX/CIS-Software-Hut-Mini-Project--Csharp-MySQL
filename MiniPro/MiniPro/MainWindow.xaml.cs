f6c1eaa11a3867c71d39cb4ebf4bed1a001d5f6a
Spectre-NSX <42746611+Spectre-NSX@users.noreply.github.com>
2018-10-15 14:50:57 +0100

    New branch to mess about with....

--- MiniPro/MiniPro/MainWindow.xaml.cs
+++ MiniPro/MiniPro/MainWindow.xaml.cs
@@ -24,5 +24,33 @@ namespace MiniPro
         {
             InitializeComponent();
         }
+        private void BtnLaunch_Click(object sender, RoutedEventArgs e)
+        {
+            double lon1 = double.Parse(TxtLon1.Text);
+            double lon2 = double.Parse(TxtLon2.Text);
+            double lat1 = double.Parse(txtLat1.Text);
+            double lat2 = double.Parse(txtLat2.Text);
+            double R = 3958.756;
+            double val1 = (lat1 * (Math.PI / 180));
+            double val2 = (lat2 * (Math.PI / 180));
+            double val3 = ((lat2 - lat1) * (Math.PI / 180));
+            double val4 = ((lon2 - lon1) * (Math.PI / 180));
+            double vala = Math.Sin(val3 / 2) * Math.Sin(val3 / 2) + Math.Cos(val1) * Math.Cos(val2) * (Math.Sin(val4 / 2) * Math.Sin(val4 / 2));
+            double valc = 2 * Math.Atan2(Math.Sqrt(vala), Math.Sqrt(1 - vala));
+
+            double distance = valc * R;
+            double output = Math.Round(distance, 3);
+            if (distance > double.Parse(txtdistance.Text))
+            {
+                MessageBox.Show("distance greater than " + txtdistance.Text + "MILES");
+                MessageBox.Show("distance = " + output + "Miles");
+            }
+            else
+            {
+                MessageBox.Show("distance within acceptable distance " + txtdistance.Text + "MILES");
+                MessageBox.Show("distance = " + output + "Miles");
+            }
+        }
+
     }
 }
