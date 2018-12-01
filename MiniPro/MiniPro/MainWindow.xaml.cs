4d8701e1463ff6c12171502183cd189b4e66bcc7
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2018-12-01 21:09:55 +0000

    Added error handling for mysql database connection

--- MiniPro/MiniPro/MainWindow.xaml.cs
+++ MiniPro/MiniPro/MainWindow.xaml.cs
@@ -42,6 +42,7 @@ namespace MiniPro
         string selectedCategoriesString;
         string ageSelectionQuery;
         string postcodeVerify;
+        bool isConn = false;
 
         public MainWindow()
         {
@@ -52,8 +53,9 @@ namespace MiniPro
                                       + "database=" + settings.mysql_database;
             // Create New connection
             conn = new MySqlConnection(connectionString);
-            
- 
+
+           
+
         }
         
 
@@ -61,150 +63,168 @@ namespace MiniPro
         private void BtnPostcode_Click(object sender, RoutedEventArgs e)
         {
             // Establish new connection
+
+
+
+
+
             MySqlCommand command = conn.CreateCommand();
 
-            // Try catch block for returning results
-            try
-            {
+                // Try catch block for returning results
+                try
+                {
+
+                    //Open connecting using conn.
+                    conn.Open();
                 
-                //Open connecting using conn.
-                conn.Open();
+
 
                 // Assign postcode string from postcodeBox
                 // This needs error handling.. somehow xD
                 postcodeInput = postcodeBox.Text;
 
 
-                // If postcode is not entered. Show message box
-                if(postcodeInput == "")
-                {
-                    MessageBox.Show("Please enter a postcode", "Warning");
-                    return;
-                }
-                
-                // Assign command string - Take postcode, get long and lat
-                commandString = "SELECT longitude, latitude, postcode FROM postcodes WHERE postcode='" + postcodeInput + "';";
-
-                //Assign Command using the commandString declared above
-                command.CommandText = commandString;
-
-                // Declare reader using command...
-                MySqlDataReader myReader = command.ExecuteReader();
-
-                // If reader is running, assign long and lat to local variables
-                postcodeVerify = null;
-                if (myReader.Read())
-                {
-                    
-                    longitude = (double)myReader[0];
-                    latitude = (double)myReader[1];
-                    // Add postcode as variable. This is used to check if the postcode entered is in the table
-                    postcodeVerify = (string)myReader[2];
-                   
-                }
-
-                // Close Reader
-                myReader.Close();
-
-                // Check if returned postcode is null, if it is, show warning then return
-                if (postcodeVerify == null)
-                {
-                    MessageBox.Show("Postcode is not a valid postcode", "Warning");
-                    return;
-                }
-                
-                // If Miles is checked unit = In miles, else it's in KM
-                if ((bool)miles.IsChecked)
-                {
-                    unitMulti = 3958.756;
-                    unit = "Miles";
-                }
-                else if ((bool)km.IsChecked)
-                {
-                    unitMulti = 6371.0002161;
-                    unit = "Kilometers";
-                }
-                
-                // If no age is checked...
-                if (!(bool)Nursery.IsChecked && !(bool)Primary.IsChecked && !(bool)Secondary.IsChecked && !(bool)None.IsChecked)
-                {
-                    MessageBox.Show("Please select an age", "Warning");
-                    return;
-                }
-
-                // If No categories are selected... Show messagebox asking to select service(s)
-                if (selectedCategories == null)
-                {
-                    MessageBox.Show("Please select some services", "Warning");
-                    return;
-                }
-
-                // Query string for user entered postcode
-                commandString2 = "SELECT c.categoryName AS 'Service Type', s.serviceName AS 'Service Name', CONCAT(s.street, ', ', s.city, ', ', s.postcode) AS Address, s.telNo AS 'Telephone Number', ROUND((" + unitMulti + "* acos( cos( radians(" + latitude + ") ) * cos( radians(p.latitude) ) * cos( radians(p.longitude) - radians(" + longitude + ") ) + sin( radians(" + latitude + ") ) * sin( radians(p.latitude) ) ) ),2) AS Distance" + unit + " FROM postcodes p, services s, categories c WHERE c.categoryId IN "+ @selectedCategoriesString + " AND p.postcode = s.postcode AND c.categoryID = s.categoryID HAVING Distance" + unit + "<  " + dst + " ORDER BY Distance" + unit + " ASC;";
-                
-                // Set command using commandString2
-                command.CommandText = commandString2;
-                // Open new reader
-                MySqlDataReader myReader2 = command.ExecuteReader();
-
-                // If reader returns no results. Set datagrid to empty, show messagebox, close reader and return
-                if (!myReader2.Read())
-                {
-                    dataGrid1.DataContext = null;
-                    MessageBox.Show("No services were found within the distance specified", "Notice");                  
-                    myReader2.Close();
-                    return;
-
-                }
-                else
-                {
-
-
-                    // Print Results to Console                
-                    while (myReader2.Read())
+                    // If postcode is not entered. Show message box
+                    if (postcodeInput == "")
                     {
-                        string row = "";
-                        for (int i = 0; i < myReader2.FieldCount; i++)
-                            row += myReader2.GetValue(i).ToString() + ", ";
-                        Console.WriteLine(row);
+                        MessageBox.Show("Please enter a postcode", "Warning");
+                        return;
+                    }
 
+                    // Assign command string - Take postcode, get long and lat
+                    commandString = "SELECT longitude, latitude, postcode FROM postcodes WHERE postcode='" + postcodeInput + "';";
+
+                    //Assign Command using the commandString declared above
+                    command.CommandText = commandString;
+
+                    // Declare reader using command...
+                    MySqlDataReader myReader = command.ExecuteReader();
+
+                    // If reader is running, assign long and lat to local variables
+                    postcodeVerify = null;
+                    if (myReader.Read())
+                    {
+
+                        longitude = (double)myReader[0];
+                        latitude = (double)myReader[1];
+                        // Add postcode as variable. This is used to check if the postcode entered is in the table
+                        postcodeVerify = (string)myReader[2];
 
                     }
 
+                    // Close Reader
+                    myReader.Close();
 
-                    // Close reader
-                    myReader2.Close();
+                    // Check if returned postcode is null, if it is, show warning then return
+                    if (postcodeVerify == null)
+                    {
+                        MessageBox.Show("Postcode is not a valid postcode", "Warning");
+                        return;
+                    }
 
-                    // Push results to datgrid
-                    MySqlCommand cmdSel = new MySqlCommand(commandString2, conn);
-                    DataTable dt = new DataTable();
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
-                    da.Fill(dt);
+                    // If Miles is checked unit = In miles, else it's in KM
+                    if ((bool)miles.IsChecked)
+                    {
+                        unitMulti = 3958.756;
+                        unit = "Miles";
+                    }
+                    else if ((bool)km.IsChecked)
+                    {
+                        unitMulti = 6371.0002161;
+                        unit = "Kilometers";
+                    }
 
-                    dataGrid1.DataContext = dt;
+                    // If no age is checked...
+                    if (!(bool)Nursery.IsChecked && !(bool)Primary.IsChecked && !(bool)Secondary.IsChecked && !(bool)None.IsChecked)
+                    {
+                        MessageBox.Show("Please select an age", "Warning");
+                        return;
+                    }
 
-                    dataGrid1.Columns[1].Width = 200;
-                    dataGrid1.Columns[2].Width = 280;
+                    // If No categories are selected... Show messagebox asking to select service(s)
+                    if (selectedCategories == null)
+                    {
+                        MessageBox.Show("Please select some services", "Warning");
+                        return;
+                    }
+
+                    // Query string for user entered postcode
+                    commandString2 = "SELECT c.categoryName AS 'Service Type', s.serviceName AS 'Service Name', CONCAT(s.street, ', ', s.city, ', ', s.postcode) AS Address, s.telNo AS 'Telephone Number', ROUND((" + unitMulti + "* acos( cos( radians(" + latitude + ") ) * cos( radians(p.latitude) ) * cos( radians(p.longitude) - radians(" + longitude + ") ) + sin( radians(" + latitude + ") ) * sin( radians(p.latitude) ) ) ),2) AS Distance" + unit + " FROM postcodes p, services s, categories c WHERE c.categoryId IN " + @selectedCategoriesString + " AND p.postcode = s.postcode AND c.categoryID = s.categoryID HAVING Distance" + unit + "<  " + dst + " ORDER BY Distance" + unit + " ASC;";
+
+                    // Set command using commandString2
+                    command.CommandText = commandString2;
+                    // Open new reader
+                    MySqlDataReader myReader2 = command.ExecuteReader();
+
+                    // If reader returns no results. Set datagrid to empty, show messagebox, close reader and return
+                    if (!myReader2.Read())
+                    {
+                        dataGrid1.DataContext = null;
+                        MessageBox.Show("No services were found within the distance specified", "Notice");
+                        myReader2.Close();
+                        return;
+
+                    }
+                    else
+                    {
+
+
+                        // Print Results to Console                
+                        while (myReader2.Read())
+                        {
+                            string row = "";
+                            for (int i = 0; i < myReader2.FieldCount; i++)
+                                row += myReader2.GetValue(i).ToString() + ", ";
+                            Console.WriteLine(row);
+
+
+                        }
+
+
+                        // Close reader
+                        myReader2.Close();
+
+                        // Push results to datgrid
+                        MySqlCommand cmdSel = new MySqlCommand(commandString2, conn);
+                        DataTable dt = new DataTable();
+                        MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+                        da.Fill(dt);
+
+                        dataGrid1.DataContext = dt;
+
+                        dataGrid1.Columns[1].Width = 200;
+                        dataGrid1.Columns[2].Width = 280;
+
+                    }
+
+                
 
                 }
 
-            }
-
-            //MySQL Error Handling
-            catch (MySqlException ex)
-            {
-                Console.Error.WriteLine("Error: {0}", ex.ToString());
-                conn = null;
-            }
-
-            // Close MySQL Connection
-            finally
-            {
-                Console.WriteLine("Closing Connection...");
-                if (conn != null)
+                //MySQL Error Handling
+                catch (MySqlException ex)
                 {
-                    conn.Close();
+
+                Console.Error.WriteLine("Error: {0}", ex.ToString());
+                    conn = null;
+                   
                 }
+                
+
+                // Close MySQL Connection
+                finally
+                {
+                    Console.WriteLine("Closing Connection...");
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                else
+                {
+                    MessageBox.Show("Cannot connect to database  :(\n Press OK to close", "Critical Error");
+                    System.Windows.Application.Current.Shutdown();
+                }
+
             }
         }
 
@@ -283,6 +303,11 @@ namespace MiniPro
                 if (conn != null)
                 {
                     conn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Cannot connect to database  :(\n Press OK to close", "Critical Error");
+                    System.Windows.Application.Current.Shutdown();
                 }
             }
         }
